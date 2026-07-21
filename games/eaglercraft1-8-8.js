974aa9b0b8e43bb931302ced6733b8c5fa2eeaac
Micah Dennert <220787835+OverlineDev@users.noreply.github.com>
2026-07-21 16:43:32 -0500

    Add Eaglercraft 1.8.8 game metadata

---
+++ games/eaglercraft1-8-8.js
@@ -0,0 +1,7 @@
+{
+  "title": "Eaglercraft 1.8.8",
+  "genre": "Sandbox",
+  "image": "/thumbnails/offline-game.jpg",
+  "url": "/games-files/eaglercraft1-8-8.html",
+  "description": "Minecraft 1.8.8 directly in your browser!."
+}
